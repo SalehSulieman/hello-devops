df1b00a5f5eb565eb2ee63cb771832e66aa0241f
Saleh <skhaledsulieman@gmail.com>
2025-08-14 19:40:02 +0300

    feat: update homepage message to v2

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+return "Hello from DevOps v2 ✨"
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps ✨"
+    return "Hello from DevOps v2 ✨"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
